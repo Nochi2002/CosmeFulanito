0503020998a0c04cb2e62375547c88937367fbf3
Nahuel <nahuel.chino@davinci.edu.ar>
2025-11-19 12:19:26 -0300

    Arreglando la mrd de render

--- run.py
+++ run.py
@@ -5,7 +5,10 @@ from app.models import db
 # nos va a servir para cuando estemos defendiendo, capaz q te olvidas como lo hiciste por los nervios nashe
 app = create_app()
 
+with app.app_context():
+    db.create_all()
+# ------------------------------
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all() 
+    # Esto solo corre en tu PC
     app.run(debug=True, port=5000)
